4f139e279e2b6b6abcc45099a85b09759e71b125
informalboi <733416@student.dusd.net>
2026-02-10 11:42:29 -0800

    twer
    gdsrf

--- P4PlayerControlRoman/Assets/Scripts/PlayerController.cs
+++ P4PlayerControlRoman/Assets/Scripts/PlayerController.cs
@@ -7,17 +7,18 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-    private float speed = 5.0f;
-    private float turnSpeed;
+    private float speed = 25.0f;
+    private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
     // Update is called once per frame
     void Update()
     {
-        // Moves the vehicle forward based on vertical input
+      
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-
+        // Moves the vehicle forward based on vertical input
+       
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
