a229552ff2cb312f294b5460f5c5256d3befc218
informalboi <733416@student.dusd.net>
2026-02-24 19:48:49 -0800

    fyhgh

--- P4PlayerControlRoman/Assets/Scripts/MoveForward.cs
+++ P4PlayerControlRoman/Assets/Scripts/MoveForward.cs
@@ -6,12 +6,12 @@ public class MoveForward : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 }
